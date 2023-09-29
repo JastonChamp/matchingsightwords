10a5d87714c2439acf862dc891860bc77ada41fc
JastonChamp <142294191+JastonChamp@users.noreply.github.com>
2023-09-29 14:22:48 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
 // script.js
-const sightWords = [
-  'a', 'about', 'above', 'again', 'all', 'also', 'are', 'be', 'came', 'day', 'do',
-  'does', 'for', 'go', 'he', 'her', 'his', 'how', 'I', 'in', 'into', 'is', 'it',
-  'know', 'many', 'name', 'not', 'now', 'of', 'on', ' one', 'over', 'said', 'she',
-  'so', 'some', 'story', 'the', 'their', 'then', 'there', 'this', 'to', 'too',
-  'want', 'was', 'were', 'what', 'when', 'white'
+const words = [
+  'a', 'about', 'above', 'again', 'all', 'also', 'are', 'be', 'came', 'day',
+  'do', 'does', 'for', 'go', 'he', 'her', 'his', 'how', 'I', 'in', 'into',
+  'is', 'it', 'know', 'many', 'name', 'not', 'now', 'of', 'on', 'one', 'over',
+  'said', 'she', 'so', 'some', 'story', 'the', 'their', 'then', 'there', 'this',
+  'to', 'too', 'want', 'was', 'were', 'what', 'when', 'white'
 ];
 
-const cardsContainer = document.getElementById('cards-container');
-const startButton = document.getElementById('start-button');
+const shuffledWords = [...words, ...words].sort(() => 0.5 - Math.random());
 
-let cards = [];
+const grid = document.querySelector('.grid');
 let flippedCards = [];
 let matchedWords = [];
 
@@ -18,69 +17,34 @@ function createCard(word) {
   const card = document.createElement('div');
   card.classList.add('card');
   card.dataset.word = word;
-  card.textContent = word; // Display the word on the card
   card.addEventListener('click', () => flipCard(card));
   return card;
 }
 
-function shuffleArray(array) {
-  for (let i = array.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [array[i], array[j]] = [array[j], array[i]];
-  }
-}
-
 function flipCard(card) {
   if (flippedCards.length < 2 && !flippedCards.includes(card) && !matchedWords.includes(card.dataset.word)) {
-    card.classList.add('flipped');
+    card.classList.toggle('flipped');
     flippedCards.push(card);
 
     if (flippedCards.length === 2) {
-      const [card1, card2] = flippedCards;
-      if (card1.dataset.word === card2.dataset.word) {
-        matchedWords.push(card1.dataset.word);
-        flippedCards = [];
-
-        if (matchedWords.length === sightWords.length) {
-          alert('Congratulations! You matched all the words.');
-          startButton.disabled = false;
+      const [firstCard, secondCard] = flippedCards;
+      if (firstCard.dataset.word === secondCard.dataset.word) {
+        matchedWords.push(firstCard.dataset.word);
+        if (matchedWords.length === words.length) {
+          alert('Congratulations! You matched all the words!');
         }
       } else {
         setTimeout(() => {
-          card1.classList.remove('flipped');
-          card2.classList.remove('flipped');
-          flippedCards = [];
+          firstCard.classList.remove('flipped');
+          secondCard.classList.remove('flipped');
         }, 1000);
       }
+      flippedCards = [];
     }
   }
 }
 
-function startGame() {
-  startButton.disabled = true;
-  cardsContainer.innerHTML = '';
-  matchedWords = [];
-  flippedCards = [];
-  cards = [];
-
-  shuffleArray(sightWords.slice(0, 5)); // Take 5 random words
-  sightWords.slice(0, 5).forEach(word => {
-    cards.push(createCard(word));
-    cards.push(createCard(word));
-  });
-
-  shuffleArray(cards);
-
-  const cardRow1 = document.createElement('div');
-  cardRow1.classList.add('card-row');
-  const cardRow2 = document.createElement('div');
-  cardRow2.classList.add('card-row');
-
-  cards.slice(0, 5).forEach(card => cardRow1.appendChild(card));
-  cards.slice(5, 10).forEach(card => cardRow2.appendChild(card));
-
-  cardsContainer.appendChild(cardRow1);
-  cardsContainer.appendChild(cardRow2);
-}
-
-startButton.addEventListener('click', startGame);
+shuffledWords.forEach((word) => {
+  const card = createCard(word);
+  grid.appendChild(card);
+});
