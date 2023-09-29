fcf61b4edd54de4ae9bd4d68217aa0d43ba9bba2
JastonChamp <142294191+JastonChamp@users.noreply.github.com>
2023-09-29 14:40:50 +0800

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,8 @@ const sightWords = [
 
 const cardContainer = document.querySelector('.card-row');
 const startButton = document.getElementById('start-button');
-
+let flippedCards = [];
+let matchedCards = [];
 let shuffledWords = [];
 
 function shuffleArray(array) {
@@ -28,24 +29,48 @@ function createCards() {
     const card = document.createElement('div');
     card.classList.add('card');
     card.dataset.word = word;
-    card.textContent = word; // Set the sight word as the text content
-    card.addEventListener('click', flipCard);
+    card.textContent = 'Click to reveal';
+    card.addEventListener('click', () => flipCard(card));
     cardContainer.appendChild(card);
   }
 }
 
 // Handle card click event
-function flipCard(event) {
-  const card = event.target;
-  card.classList.add('flipped');
-  card.removeEventListener('click', flipCard);
+function flipCard(card) {
+  if (flippedCards.length < 2 && !flippedCards.includes(card)) {
+    card.textContent = card.dataset.word;
+    flippedCards.push(card);
+
+    if (flippedCards.length === 2) {
+      const [card1, card2] = flippedCards;
+      if (card1.dataset.word === card2.dataset.word) {
+        matchedCards.push(card1, card2);
+        flippedCards = [];
+      } else {
+        setTimeout(() => {
+          card1.textContent = 'Click to reveal';
+          card2.textContent = 'Click to reveal';
+          flippedCards = [];
+        }, 1000);
+      }
+    }
+
+    if (matchedCards.length === shuffledWords.length) {
+      // All cards are matched, reset the game
+      matchedCards = [];
+      startButton.textContent = 'Next Set of Words';
+      startButton.disabled = false;
+    }
+  }
 }
 
 // Handle start button click event
 startButton.addEventListener('click', () => {
+  startButton.disabled = true;
   // Shuffle the sight words
-  shuffledWords = shuffleArray(sightWords);
+  shuffledWords = shuffleArray(sightWords.slice(0, 10)); // Take the first 10 words
   createCards();
 });
 
-// ... (rest of the code)
+// Initialize the game
+createCards();
