f15f19c0d9c4ed4d79bb31500dcd5e800c648807
JastonChamp <142294191+JastonChamp@users.noreply.github.com>
2023-09-29 14:36:56 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,24 @@
 // JavaScript code
-const words = [
+const sightWords = [
   'a', 'about', 'above', 'again', 'all', 'also', 'are', 'be', 'came', 'day',
-  'do', 'does', 'for', 'go', 'he', 'her', 'his', 'how', 'I', 'in', 'into',
-  'is', 'it', 'know', 'many', 'name', 'not', 'now', 'of', 'on', 'one', 'over',
-  'said', 'she', 'so', 'some', 'story', 'the', 'their', 'then', 'there', 'this',
-  'to', 'too', 'want', 'was', 'were', 'what', 'when', 'white'
+  'do', 'does', 'for', 'go', 'he', 'her', 'his', 'how', 'I', 'in', 'into', 'is',
+  'it', 'know', 'many', 'name', 'not', 'now', 'of', 'on', 'one', 'over', 'said',
+  'she', 'so', 'some', 'story', 'the', 'their', 'then', 'there', 'this', 'to',
+  'too', 'want', 'was', 'were', 'what', 'when', 'white'
 ];
 
-const shuffledWords = shuffleArray([...words, ...words]); // Duplicate words for matching
 const cardContainer = document.querySelector('.card-row');
 const startButton = document.getElementById('start-button');
 
-let cardsFlipped = 0;
-let firstCard, secondCard;
-let lockBoard = false;
+let shuffledWords = [];
 
-// Shuffle an array using the Fisher-Yates algorithm
 function shuffleArray(array) {
-  for (let i = array.length - 1; i > 0; i--) {
+  const shuffled = array.slice();
+  for (let i = shuffled.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
-    [array[i], array[j]] = [array[j], array[i]];
+    [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
   }
-  return array;
+  return shuffled;
 }
 
 // Create and append cards to the DOM
@@ -31,69 +28,24 @@ function createCards() {
     const card = document.createElement('div');
     card.classList.add('card');
     card.dataset.word = word;
-    card.textContent = 'Click to reveal';
+    card.textContent = word; // Set the sight word as the text content
     card.addEventListener('click', flipCard);
     cardContainer.appendChild(card);
   }
 }
 
-// Flip a card to reveal its word
-function flipCard() {
-  if (lockBoard) return;
-  if (this === firstCard) return;
-
-  this.classList.add('flipped');
-
-  if (!firstCard) {
-    firstCard = this;
-    return;
-  }
-
-  secondCard = this;
-  checkForMatch();
+// Handle card click event
+function flipCard(event) {
+  const card = event.target;
+  card.classList.add('flipped');
+  card.removeEventListener('click', flipCard);
 }
 
-// Check if two flipped cards match
-function checkForMatch() {
-  if (firstCard.dataset.word === secondCard.dataset.word) {
-    disableCards();
-    cardsFlipped += 2;
-    if (cardsFlipped === shuffledWords.length) {
-      alert('Congratulations! You matched all the cards.');
-    }
-  } else {
-    unflipCards();
-  }
-}
-
-// Disable matched cards
-function disableCards() {
-  firstCard.removeEventListener('click', flipCard);
-  secondCard.removeEventListener('click', flipCard);
-  resetBoard();
-}
-
-// Unflip cards if they don't match
-function unflipCards() {
-  lockBoard = true;
-  setTimeout(() => {
-    firstCard.classList.remove('flipped');
-    secondCard.classList.remove('flipped');
-    resetBoard();
-  }, 1000);
-}
-
-// Reset the board for the next turn
-function resetBoard() {
-  [firstCard, secondCard] = [null, null];
-  lockBoard = false;
-}
-
-// Add click event listener to the start button to create cards
+// Handle start button click event
 startButton.addEventListener('click', () => {
+  // Shuffle the sight words
+  shuffledWords = shuffleArray(sightWords);
   createCards();
-  startButton.style.display = 'none'; // Hide the start button
 });
 
-// Initialize the game
-createCards();
+// ... (rest of the code)
