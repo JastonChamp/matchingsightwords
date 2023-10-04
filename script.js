30af9cd1dc959346b7a953bd784a236f422b5fc8
JastonChamp <142294191+JastonChamp@users.noreply.github.com>
2023-10-04 14:26:59 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,104 +1,81 @@
 // JavaScript code
-const sightWords = [
-  'a', 'about', 'above', 'again', 'all', 'also', 'are', 'be', 'came', 'day',
-  'do', 'does', 'for', 'go', 'he', 'her', 'his', 'how', 'I', 'in', 'into', 'is',
-  'it', 'know', 'many', 'name', 'not', 'now', 'of', 'on', 'one', 'over', 'said',
-  'she', 'so', 'some', 'story', 'the', 'their', 'then', 'there', 'this', 'to',
-  'too', 'want', 'was', 'were', 'what', 'when', 'white'
+const wheel = document.querySelector('.wheel');
+const challengingWordsSets = [
+    ['a', 'about', 'above', 'again', 'all'],
+    ['also', 'are', 'be', 'came', 'day'],
+    ['do', 'does', 'for', 'go', 'he'],
+    ['her', 'his', 'how', 'I', 'in'],
+    ['into', 'is', 'it', 'know', 'many'],
+    ['name', 'not', 'now', 'of', 'on'],
+    ['one', 'over', 'said', 'it', 'know'],
+    ['many', 'name', 'not', 'now', 'of'],
+    ['on', 'one', 'over', 'said', 'she'],
+    ['so', 'some', 'story', 'the', 'their'],
+    ['then', 'there', 'this', 'to', 'too'],
+    ['want', 'was', 'were', 'what', 'when', 'white']
 ];
 
-const cardContainer = document.querySelector('.card-row');
-const startButton = document.getElementById('start-button');
-let flippedCards = [];
-let matchedCards = [];
-let shuffledSets = [];
 let currentSet = 0;
-
-// Shuffle the sets of 5 words with 2 cards each
-function shuffleSets() {
-  const sets = [];
-  const wordsCopy = sightWords.slice();
-
-  while (wordsCopy.length > 0) {
-    const set = [];
-    for (let i = 0; i < 5; i++) {
-      const word = wordsCopy.pop();
-      set.push(word, word); // Add 2 cards with the same word
-    }
-    sets.push(set);
-  }
-
-  return sets;
-}
+let shuffledWords = [];
 
 function shuffleArray(array) {
-  const shuffled = array.slice();
-  for (let i = shuffled.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
-  }
-  return shuffled;
-}
-
-// Create and append cards to the DOM
-function createCards() {
-  cardContainer.innerHTML = '';
-  const set = shuffledSets[currentSet];
-  for (let word of set) {
-    const card = document.createElement('div');
-    card.classList.add('card');
-    card.dataset.word = word;
-    card.textContent = 'Click to reveal';
-    card.addEventListener('click', () => flipCard(card));
-    cardContainer.appendChild(card);
-  }
-}
-
-// Handle card click event
-function flipCard(card) {
-  if (flippedCards.length < 2 && !flippedCards.includes(card)) {
-    card.textContent = card.dataset.word;
-    flippedCards.push(card);
-
-    if (flippedCards.length === 2) {
-      const [card1, card2] = flippedCards;
-      if (card1.dataset.word === card2.dataset.word) {
-        matchedCards.push(card1, card2);
-        flippedCards = [];
-        // Indicate correct match with background color
-        card1.classList.add('matched');
-        card2.classList.add('matched');
-      } else {
-        setTimeout(() => {
-          card1.textContent = 'Click to reveal';
-          card2.textContent = 'Click to reveal';
-          flippedCards = [];
-        }, 1000);
-      }
+    const shuffled = array.slice();
+    for (let i = shuffled.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
     }
-
-    if (matchedCards.length === sightWords.length) {
-      // All cards are matched, go to the next set
-      matchedCards = [];
-      currentSet++;
-      if (currentSet < shuffledSets.length) {
-        startButton.textContent = 'Next Set of Words';
-        startButton.disabled = false;
-      } else {
-        startButton.textContent = 'Game Over';
-        startButton.disabled = true;
-      }
-    }
-  }
+    return shuffled;
 }
 
-// Handle start button click event
-startButton.addEventListener('click', () => {
-  startButton.disabled = true;
-  shuffledSets = shuffleSets();
-  currentSet = 0;
-  createCards();
+// Create and append slots to the DOM
+function createSlots() {
+    wheel.innerHTML = '';
+    const wordSet = challengingWordsSets[currentSet];
+    for (let word of wordSet) {
+        for (let i = 0; i < 2; i++) {
+            const slot = document.createElement('div');
+            slot.className = 'slot';
+            slot.textContent = 'Click to reveal';
+            wheel.appendChild(slot);
+        }
+    }
+    shuffledWords = shuffleArray(wordSet.slice().concat(wordSet.slice())); // Add two of each word
+}
+
+const slots = document.querySelectorAll('.slot');
+let currentSlot = 0;
+
+document.getElementById('spinButton').addEventListener('click', () => {
+    createSlots();
+    currentSlot = 0;
+    spin();
 });
 
-// Initialize the game
-createCards();
+function spin() {
+    let shuffleCount = 0;
+    let lastRandom = 0;
+
+    const shuffleEffect = setInterval(() => {
+        slots[lastRandom].style.display = 'none';
+        const randomSlot = Math.floor(Math.random() * slots.length);
+        slots[randomSlot].style.display = 'flex';
+        lastRandom = randomSlot;
+        shuffleCount++;
+        if (shuffleCount > 20) {
+            clearInterval(shuffleEffect);
+            slots[lastRandom].style.display = 'none';
+            slots[currentSlot].style.display = 'flex';
+        }
+    }, 100);
+
+    setTimeout(() => {
+        const randomSlot = Math.floor(Math.random() * slots.length);
+        slots[currentSlot].style.display = 'none';
+        slots[randomSlot].style.display = 'flex';
+        currentSlot = randomSlot;
+    }, 2500);
+}
+
+// Initialize the spinner
+createSlots();
+spin();
